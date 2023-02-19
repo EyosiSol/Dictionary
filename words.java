bc6c81a010c248edf6de699fa1a51253b25fa7a3
EyosiSol <115923891+EyosiSol@users.noreply.github.com>
2023-02-19 17:55:30 +0300

    Create words.java

---
+++ words.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+public class words{
+    HashMap<String,String> hMap;
+    public  HashMap<String,String> addToDictionary(){
+
+        hMap = new HashMap<>();
+        //A
+        hMap.put("acute", "ሀይለኛ ፤ አስቸጋሪ");
+        hMap.put("almighty", "ሀያሉ");
+        hMap.put("apostacy", "ሀይማኖትን መተው");
+        hMap.put("asperity", "ሀይለ ቃል");
+        hMap.put("assignee", "ሀላፊነት የተሰጠው");
+        hMap.put("arsenic", "ሀይለኛ መርዝ");
+        hMap.put("abase", "ማፈር ፤ መዋረድ");
+        hMap.put("ability", "ችሎታ፤ ተሰጣኦ");
+        hMap.put("ache", "ህመም ፤ ዉጋት");
+        hMap.put("advisory", "አማካሪ");
+        //B
+        hMap.put("band dog", "ሀይለኛ ውሻ");
+        hMap.put("bile", "ሀሞት");
+        hMap.put("bogey", "ፍርሃት");
+        hMap.put("brain wash", "ሀሳብ አስለወጠ");
+        hMap.put("broken hearted", "ሀዘን የተጫነው");
+        hMap.put("bull headed", "ሀሳበ ግትር፤ መንቻካ");
+        hMap.put("bulldog", " ሀይለኛ ውሻ");
+        hMap.put("bum", " ሀላፊነት የሚሰማው፤ ቂጥ");
+        hMap.put("beggar", "ለማኝ");
+        hMap.put("back", "ጀርባ");
+        //Words Starts with "C"
+        hMap.put("calve", "ወልደት"+"(ጥጃ)");
+        hMap.put("camp", "ሰፈረ");
+        hMap.put("canon", "ህግ ፤ ደንብ");
+        hMap.put("canton", "ግዛት");
+        hMap.put("certify", "አረጋገጠ ፤ መሰከረ");
+        hMap.put("char", "አሳረረ");
+        hMap.put("chisel", "መሮ");
+        hMap.put("civilly", "የሰለጠነ ፤ መልካም");
+        hMap.put("claw", "ጥፍር");
+        hMap.put("conductor", "መሪ ፤ አስተላላፊ");
+
+        return hMap;
+    }
+}
